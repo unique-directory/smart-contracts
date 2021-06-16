3b5f61dd0f3ff867b323cf9bc27de057e0274e76
aramalipoor <aram.alipoor@gmail.com>
2021-06-16 22:04:39 +0200

    chore: upload and add metadata for gods collection

--- scripts/submission-approve-bulk.js
+++ scripts/submission-approve-bulk.js
@@ -10,14 +10,19 @@ async function main() {
 
   // TODO Build a CSV parser to read from a file
   const hashes = [
-    // 'QmPGpPPtZxtCpKfqLsrdg4YvRqaT5mg46FxppoVA21Qr9V',
-    // 'QmW9KdEa9vAgw6kqLC73kCKeSDoxjW5jag6XcvYzxjwa6R',
-    // 'QmUXSH3S42iPpuqD41aZm7W9mNzZffmRkkJhK2m9bv2PEm',
-    // 'QmPWcsb9KjnofcU7D6mz7HNQwyuQGxMnwZiknb1HJBYXHj',
-    // 'QmafZo9P38eduXB2XBQ67RrmPBczUYetrBf7s4KgHp5qQa',
-    // 'QmPi3xRFJ2pGpTQtrbWQSdMWqoRaKrDpUzaMAfVXniaQGZ',
-    // 'QmU26QRSnnwK4Su2Md3aU6bbXjNfEKziJKhStwJeQhjrcZ',
-    'QmWCPjSf3n8PCfMZBrCCd8LZMXFVjt4hzcM5WJEn965djh'
+    'QmbM9GSzoRCDchirf7DGh5uPQrCAoD8fGtEQTDS9KEoekN',
+    'QmdwN3HKoWtk25YfNcLGTsNHipnMNicziyCveRSxc685k6',
+    'Qmd8Rrmw79idKzctxfvowCg7jFA5pStCPK4ek1qBgH8HDZ',
+    'QmcoRSjkpBgnk9RNV79t3Gho7D487YZ3VPk8bdgxZu3WKP',
+    'QmNdZxeZKLmU9Gj5iDQRaLWP6pe8ucHabqFKRGCCtpAUsj',
+    'QmeQ5PxiSdH7w4zaaCXR6vsrJGRZG54ZZoopat7mTkfnzC',
+    'QmcNDKz5GfjRxqyd9PuXei19DWeKgA1SSk6Pohak3BzAQC',
+    'QmZnMsvtWXX8EgLGrUYZaHj5aRKZEWn8rwg1wUXBX75v5p',
+    'QmU8CrXjpe7m9Eo5DJqXoz3VjkHn2aHVf8o5pMUBYVosq3',
+    'QmNNpyTvgoZ7Ruy6TMVT62McNj738aiamCev5NKF8HS9hA',
+    'QmS4cYaSpQgSpLtwu8aFHdSW4sAuT63bmGAFD11f6qaNGM',
+    'Qme4oyBXEiUWBsXnn3tBVwwR3eTZfitFzTKM9khRMQM8LZ',
+    'QmXsTUNX3cgypXwkmFiU3owjv4gvc8f4gHW5EiiWyk2XpP'
   ];
   const rewards = Array(hashes.length).fill(
     web3.utils.toWei('100')
